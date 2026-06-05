cb47cba1c85348d6a65c32e52c696427470f14be
Jamila Ben Abdelkader <Benabdeljamyla@gmail.com>
2026-06-05 13:12:35 +0100

    fix: add test profile to GestionUserApplicationTests to disable external dependencies

--- GestionUser/src/test/java/tn/vermeg/gestionuser/GestionUserApplicationTests.java
+++ GestionUser/src/test/java/tn/vermeg/gestionuser/GestionUserApplicationTests.java
@@ -2,8 +2,10 @@ package tn.vermeg.gestionuser;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GestionUserApplicationTests {
 
 	@Test
